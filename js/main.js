e87b8e28e1aa1eb98dc1fe9d0b764ff0a8bbbfad
Kat Nelson <kat98058@byui.edu>
2021-01-06 21:49:19 -0600

    JS changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-var weeks =["<li><a href''>Week 1></a><li>"];
+var week =[" <a href''>Week 1></a>"];
 
 function index(){
+    var weeks = week.toString();
     var page = document.getElementById("menu").innerHTML = weeks;
 };
