12310e8045156ad3b7e70920631134661666974e
Kat Nelson <kat98058@byui.edu>
2021-01-07 21:23:22 -0600

    adding Week 1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 
 function index(){
-    var weeks = ["<a href =''> Week 1 </a>"]
+    var weeks = ["<a href ='Week1/index.html'> Week 1 </a>"]
     document.getElementById('menu').innerHTML = weeks
     
     
