777c2f87a10a778ad706e30bdd377001de831a74
Kat Nelson <kat98058@byui.edu>
2021-01-15 18:34:06 -0600

    Week 2 Updates

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 
 function index(){
-    var weeks = ["<a href ='Week1/index.html'> Week 1 </a>"]
+    var weeks = ["<li><a href ='Week1/index.html'> Week 1 </a></li>","<li><a href ='Week2/index.html'>Week 2<a></li>"]
     document.getElementById('menu').innerHTML = weeks
     
     
