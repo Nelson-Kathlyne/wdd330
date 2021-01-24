47aa15f2d9e1506cfb8b69ecdeae993f5f076380
Kat Nelson <kat98058@byui.edu>
2021-01-23 23:29:15 -0600

    updating links

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 
 
 function index(){
-    var weeks = ["<li><a href ='Week1/index.html'> Week 1 </a></li>","<li><a href ='Week2/index.html'>Week 2<a></li>"]
+    const weeks = ["<li><a href ='Week1/index.html'> Week 1 </a></li>","<li><a href ='Week2/index.html'>Week 2<a></li>", 
+    "<li><a href = 'Week3/index.html'>Week 3<a></li>"]
     document.getElementById('menu').innerHTML = weeks
     
     
