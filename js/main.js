735b594ab63492c08826dc96fc4a364d3cda7866
Kat Nelson <kat98058@byui.edu>
2021-03-13 18:13:26 -0600

    updating js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ function index(){
     const weeks = ["<li><a href ='Week1/index.html'> Week 1 </a></li>","<li><a href ='Week2/index.html'>Week 2<a></li>", 
     "<li><a href = 'Week3/index.html'>Week 3<a></li>","<li><a href = 'Week4/index.html'>Week 4<a></li>","<li><a href = 'Week5/index.html'>Week 5<a></li>",
     "<li><a href = 'Week6/index.html'>Week 6<a></li>", "<li><a href = 'week7/index.html'>Week 7<a></li>", "<li><a href = 'Week8/index.html'>Week 8<a></li>",
-    "<li><a href = 'Week9/index.html'>Week 9<a></li>","<li><a href = 'Week9/index.html'>Week 10<a></li>"]
+    "<li><a href = 'Week9/index.html'>Week 9<a></li>","<li><a href = 'Week10/index.html'>Week 10<a></li>"]
     document.getElementById('menu').innerHTML = weeks
     
     
